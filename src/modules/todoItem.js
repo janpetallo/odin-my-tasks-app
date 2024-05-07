7c2ba4c408ac8e7c8b223dc2a6bdc953945f9f74
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-05-07 12:28:42 -0600

    todoItem class

---
+++ src/modules/todoItem.js
@@ -0,0 +1,15 @@
+
+class TodoItem {
+    constructor (title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+
+    
+}
+
+
+export { TodoItem };
