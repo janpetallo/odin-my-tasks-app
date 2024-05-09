82ac28b1403e330dd53317161b02f992983f0516
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-05-08 22:28:28 -0600

    styling changes with new task icons added earlier

--- src/modules/render.js
+++ src/modules/render.js
@@ -103,11 +103,15 @@ class Render {
         deleteIcon.classList.add('delete-icon');
         deleteIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
 
+        // wrap date, edit an delete in a div
+        const actionDiv = document.createElement('div');
+        actionDiv.classList.add('action');
+        actionDiv.appendChild(dateDiv);
+        actionDiv.appendChild(editIcon);
+        actionDiv.appendChild(deleteIcon);
         
         taskDiv.appendChild(titleDiv);
-        taskDiv.appendChild(dateDiv);
-        taskDiv.appendChild(editIcon);
-        taskDiv.appendChild(deleteIcon);
+        taskDiv.appendChild(actionDiv);
 
         this.todoList.appendChild(taskDiv);
     }
