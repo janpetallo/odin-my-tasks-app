48d38911c849167d7fdf4df9c778148a5379ce3c
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-05-10 18:04:55 -0600

    initialize edit modal for each task

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,6 +1,8 @@
 import { format, formatDistance, toDate, subDays } from "date-fns";
 import { saveProjects } from "./storage";
 
+import { initializeEditModal } from "./modal";
+
 class Render {
     constructor() {
         this.projectList = document.querySelector('.project-list');
@@ -154,6 +156,8 @@ class Render {
         const editIcon = document.createElement('div');
         editIcon.classList.add('edit-icon');
         editIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>';
+
+        initializeEditModal(editIcon, todoItem, this.projects, this);
 
         // add a delete icon to the task
         const deleteIcon = document.createElement('div');
